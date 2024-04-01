f04c6b49fc226995e381064f8ffaaf6215112a7b
kdt57 <kdt57@drexel.edu>
2024-04-01 16:29:22 -0400

    updated on connectors logic for mmWave and google coral

--- thingsboard-gateway/thingsboard_gateway/connectors/socket/bytes_socket_uplink_converter.py
+++ thingsboard-gateway/thingsboard_gateway/connectors/socket/bytes_socket_uplink_converter.py
@@ -14,6 +14,8 @@
 
 from thingsboard_gateway.connectors.socket.socket_uplink_converter import SocketUplinkConverter, log
 from thingsboard_gateway.gateway.statistics_service import StatisticsService
+import json
+import ast
 
 
 class BytesSocketUplinkConverter(SocketUplinkConverter):
@@ -34,23 +36,49 @@ class BytesSocketUplinkConverter(SocketUplinkConverter):
             self.dict_result["telemetry"] = []
             self.dict_result["attributes"] = []
 
+    
             for section in ('telemetry', 'attributes'):
                 for item in config[section]:
                     try:
-                        byte_from = item.get('byteFrom')
-                        byte_to = item.get('byteTo')
+                        # log.error("Raw data %s", data)
+                        jsondata = data.decode('utf-8')
+                        # log.error("decoded data: %s", jsondata)
+                        # jsondata = jsondata.replace("'", "\"")
+                        # log.error("jsondata: %s", jsondata)
+                        buf_dict = json.loads(jsondata)
+                        log.error("buf_dict: %s", buf_dict)
+                        # log.error("Type of dict: %s", type(buf_dict))
+                        # byte_from = item.get('byteFrom')
+                        # byte_to = item.get('byteTo')
 
-                        byte_to = byte_to if byte_to != -1 else len(data)
-                        converted_data = data[byte_from:byte_to]
+                        # byte_to = byte_to if byte_to != -1 else len(data)
+                        # converted_data = data[byte_from:byte_to]
 
-                        try:
-                            converted_data = converted_data.replace(b"\x00", b'').decode(config['encoding'])
-                        except UnicodeDecodeError:
-                            converted_data = str(converted_data)
+                        # try:
+                        #     converted_data = converted_data.replace(b"\x00", b'').decode(config['encoding'])
+                        # except UnicodeDecodeError:
+                        #     converted_data = str(converted_data)
+                        object_1 = buf_dict["frame"][0]["Object Detected 1"]
+                        score_1 = buf_dict["frame"][0]["Score 1"]
+                        object_2 = buf_dict["frame"][0]["Object Detected 2"]
+                        score_2 = buf_dict["frame"][0]["Score 2"]
+                        # log.error("converted data: %s", converted_data)
+                        # converted_data = 10
 
                         if item.get('key') is not None:
-                            self.dict_result[section].append(
-                                {item['key']: converted_data})
+
+                            if item.get('key') == "Object Detected no.1":
+                                self.dict_result[section].append(
+                                    {item['key']: object_1})
+                            elif item.get('key') == "Score of no.1":
+                                self.dict_result[section].append(
+                                    {item['key']: score_1})
+                            elif item.get('key') == "Object Detected no.2":
+                                self.dict_result[section].append(
+                                    {item['key']: object_2})
+                            elif item.get('key') == "Score of no.2":
+                                self.dict_result[section].append(
+                                    {item['key']: score_2})
                         else:
                             log.error('Key for %s not found in config: %s', config['type'], config['section_config'])
                     except Exception as e:
--- thingsboard-gateway/thingsboard_gateway/extensions/serial/custom_serial_converter.py
+++ thingsboard-gateway/thingsboard_gateway/extensions/serial/custom_serial_converter.py
@@ -13,7 +13,11 @@
 #     limitations under the License.
 
 from thingsboard_gateway.connectors.converter import Converter, log
+import numpy as np
+import time
 
+# byteBuffer = np.zeros(2**15, dtype='uint8')
+# byteBufferLength = 0
 
 class CustomSerialUplinkConverter(Converter):
     def __init__(self, config):
@@ -24,35 +28,165 @@ class CustomSerialUplinkConverter(Converter):
             'attributes': [],
             'telemetry': []
             }
+        self.byteBuffer = np.zeros(2**15, dtype='uint8')
+        self.byteBufferLength = 0
 
     def convert(self, config, data: bytes):
+        # global byteBuffer, byteBufferLength
+
+        # Constants
+        OBJ_STRUCT_SIZE_BYTES = 12;
+        BYTE_VEC_ACC_MAX_SIZE = 2**15;
+        MMWDEMO_UART_MSG_DETECTED_POINTS = 1;
+        MMWDEMO_UART_MSG_RANGE_PROFILE   = 2;
+        maxBufferSize = 2**15;
+        tlvHeaderLengthInBytes = 8;
+        pointLengthInBytes = 16;
+        magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+        
+        # Initialize variables
+        magicOK = 0 # Checks if magic number has been read
+        dataOK = 0 # Checks if the data has been read correctly
+        frameNumber = 0
+        detObj = {}
+
         keys = ['attributes', 'telemetry']
         for key in keys:
             self.result_dict[key] = []
             if self.__config.get(key) is not None:
                 for config_object in self.__config.get(key):
                     data_to_convert = data
-                    tmp = ""
-                    if config_object.get('untilDelimiter') is not None:
-                        # data_to_convert = data.split(config_object.get('untilDelimiter').encode('UTF-8'))[1]
-                        # for i in range(31, 35):
-                        #     if data_to_convert[i] != 0:
-                        #         tmp += str(data_to_convert[i])
-                        log.debug("raw data: %s", data_to_convert)
-                    
-                    if config_object.get('fromDelimiter') is not None:
-                        data_to_convert = data.split(config_object.get('fromDelimiter').encode('UTF-8'))[1]
-                    if config_object.get('toByte') is not None:
-                        to_byte = config_object.get('toByte')
-                        if to_byte == -1:
-                            to_byte = len(data) - 1
-                        data_to_convert = data_to_convert[:to_byte]
-                    if config_object.get('fromByte') is not None:
-                        from_byte = config_object.get('fromByte')
-                        data_to_convert = data_to_convert[from_byte:]
-                    converted_data = {config_object['key']: data_to_convert.decode('UTF-8')}
-                    self.result_dict[key].append(converted_data)
-                    # converted_data = tmp
-                    # self.result_dict[key].append(converted_data)
-        log.debug("Converted data: %s", self.result_dict)
+                    # log.info("data: %s", data_to_convert)
+                    byteVec = np.frombuffer(data_to_convert, dtype = 'uint8')
+                    byteCount = len(byteVec)
+
+                    # Check that the buffer is not full, and then add the data to the buffer
+                    if (self.byteBufferLength + byteCount) < maxBufferSize:
+                        self.byteBuffer[self.byteBufferLength:self.byteBufferLength + byteCount] = byteVec[:byteCount]
+                        self.byteBufferLength = self.byteBufferLength + byteCount
+                        
+                    # Check that the buffer has some data
+                    if self.byteBufferLength > 16:
+                        
+                        # Check for all possible locations of the magic word
+                        possibleLocs = np.where(self.byteBuffer == magicWord[0])[0]
+
+                        # Confirm that is the beginning of the magic word and store the index in startIdx
+                        startIdx = []
+                        for loc in possibleLocs:
+                            check = self.byteBuffer[loc:loc+8]
+                            if np.all(check == magicWord):
+                                startIdx.append(loc)
+                            
+                        # Check that startIdx is not empty
+                        if startIdx:
+                            
+                            # Remove the data before the first start index
+                            if startIdx[0] > 0 and startIdx[0] < self.byteBufferLength:
+                                self.byteBuffer[:self.byteBufferLength-startIdx[0]] = self.byteBuffer[startIdx[0]:self.byteBufferLength]
+                                self.byteBuffer[self.byteBufferLength-startIdx[0]:] = np.zeros(len(self.byteBuffer[self.byteBufferLength-startIdx[0]:]),dtype = 'uint8')
+                                self.byteBufferLength = self.byteBufferLength - startIdx[0]
+                                
+                            # Check that there have no errors with the byte buffer length
+                            if self.byteBufferLength < 0:
+                                self.byteBufferLength = 0
+                                
+                            # word array to convert 4 bytes to a 32 bit number
+                            word = [1, 2**8, 2**16, 2**24]
+                            
+                            # Read the total packet length
+                            totalPacketLen = np.matmul(self.byteBuffer[12:12+4],word)
+                            
+                            # Check that all the packet has been read
+                            if (self.byteBufferLength >= totalPacketLen) and (self.byteBufferLength != 0):
+                                magicOK = 1
+
+                    # log.info("Magic number: %s", magicOK)
+                    # If magicOK is equal to 1 then process the message
+                    if magicOK:
+                        # word array to convert 4 bytes to a 32 bit number
+                        word = [1, 2**8, 2**16, 2**24]
+                        
+                        # Initialize the pointer index
+                        idX = 0
+                        
+                        # Read the header
+                        magicNumber = self.byteBuffer[idX:idX+8]
+                        idX += 8
+                        version = format(np.matmul(self.byteBuffer[idX:idX+4],word),'x')
+                        idX += 4
+                        totalPacketLen = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+                        platform = format(np.matmul(self.byteBuffer[idX:idX+4],word),'x')
+                        idX += 4
+                        frameNumber = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+                        timeCpuCycles = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+                        numDetectedObj = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+                        numTLVs = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+                        subFrameNumber = np.matmul(self.byteBuffer[idX:idX+4],word)
+                        idX += 4
+
+                        # Read the TLV messages
+                        for tlvIdx in range(numTLVs):
+                            
+                            # word array to convert 4 bytes to a 32 bit number
+                            word = [1, 2**8, 2**16, 2**24]
+
+                            # Check the header of the TLV message
+                            tlv_type = np.matmul(self.byteBuffer[idX:idX+4],word)
+                            idX += 4
+                            tlv_length = np.matmul(self.byteBuffer[idX:idX+4],word)
+                            idX += 4
+
+                            # Read the data depending on the TLV message
+                            if tlv_type == MMWDEMO_UART_MSG_DETECTED_POINTS:
+
+                                # Initialize the arrays
+                                x = np.zeros(numDetectedObj,dtype=np.float32)
+                                y = np.zeros(numDetectedObj,dtype=np.float32)
+                                z = np.zeros(numDetectedObj,dtype=np.float32)
+                                velocity = np.zeros(numDetectedObj,dtype=np.float32)
+                                
+                                for objectNum in range(numDetectedObj):
+                                    
+                                    # Read the data for each object
+                                    x[objectNum] = self.byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                                    idX += 4
+                                    y[objectNum] = self.byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                                    idX += 4
+                                    z[objectNum] = self.byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                                    idX += 4
+                                    velocity[objectNum] = self.byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                                    idX += 4
+                                
+                                # Store the data in the detObj dictionary
+                                detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity}
+                                dataOK = 1
+                                
+                
+                        # Remove already processed data
+                        if idX > 0 and self.byteBufferLength>idX:
+                            shiftSize = totalPacketLen
+                            
+                                
+                            self.byteBuffer[:self.byteBufferLength - shiftSize] = self.byteBuffer[shiftSize:self.byteBufferLength]
+                            self.byteBuffer[self.byteBufferLength - shiftSize:] = np.zeros(len(self.byteBuffer[self.byteBufferLength - shiftSize:]),dtype = 'uint8')
+                            self.byteBufferLength = self.byteBufferLength - shiftSize
+                            
+                            # Check that there are no errors with the buffer length
+                            if self.byteBufferLength < 0:
+                                self.byteBufferLength = 0         
+
+                                        
+
+                        now = int(round(time.time()*1000))
+                        numDetectedObj_in_int = int(numDetectedObj)
+                        converted_data = {config_object['key']: numDetectedObj_in_int, "ts": now, "values": {"Number of detected object:": numDetectedObj_in_int}}
+                        self.result_dict[key].append(converted_data)
+                        log.info("Converted data: %s", self.result_dict)
+        
         return self.result_dict
